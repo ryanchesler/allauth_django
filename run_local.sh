3a50d8977be3baded37bec695fe834607c20cc7c
arifur <arifcse7@gmail.com>
2017-08-04 23:45:22 +0600

    modified run file

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,3 @@
 docker stop allauth_django
 docker rm allauth_django
-docker run -it -p 443:443 --name allauth_django -v /home/arifur/workspace_python/allauth_app:/code  allauth_django
+docker run -it -p 443:443 --name allauth_django -v /home/arifur/workspace_python/allauth_django:/code  allauth_django
